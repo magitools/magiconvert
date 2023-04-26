3727914a9b7da534b477eac9cff71cdbc73d98fe
Matteo Gassend <mgassend@gmail.com>
2023-04-26 14:34:56 +0200

    add: link for hash page

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -4,5 +4,10 @@
 </script>
 <AppBar>
 	<svelte:fragment slot="lead"><a href="/">Magiconvert</a></svelte:fragment>
-  <svelte:fragment slot="trail"><a href="/convert">Convert</a></svelte:fragment>
+  <svelte:fragment slot="trail">
+	<div class=" space-x-2">
+		<a href="/convert">Convert</a>
+		<a href="/hash">Hash</a>
+	</div>
+  </svelte:fragment>
 </AppBar>
