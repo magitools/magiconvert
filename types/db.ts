2a72355f328d71e9f83d8199156c2066e1809c42
Matteo Gassend <mgassend@gmail.com>
2024-04-27 17:26:53 +0200

    add: new types for db

--- types/db.ts
+++ types/db.ts
@@ -1,41 +1,49 @@
-import type { ColumnType } from 'kysely'
+import type { ColumnType } from "kysely";
 
-export type Generated<T> =
-  T extends ColumnType<infer S, infer I, infer U>
+export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
   ? ColumnType<S, I | undefined, U>
-  : ColumnType<T, T | undefined, T>
+  : ColumnType<T, T | undefined, T>;
+
+export interface Blurs {
+  created_at: string;
+  id: number;
+  updated_at: string;
+  user_id: number;
+  value: string;
+}
 
 export interface Images {
-  created_at: string
-  id: number
-  original_path: string
-  converted_path: string
-  updated_at: string
-  user_id: number
+  converted_path: string | null;
+  created_at: string;
+  id: number;
+  original_path: string | null;
+  updated_at: string;
+  user_id: number;
 }
 
 export interface Transactions {
-  amount: Generated<number>
-  created_at: string
-  id: number
-  quantity: Generated<number>
-  updated_at: string
-  user_id: number
+  amount: Generated<number>;
+  created_at: string;
+  id: number;
+  quantity: Generated<number>;
+  updated_at: string;
+  user_id: number;
 }
 
 export interface Users {
-  created_at: string
-  credits: number
-  email: string
-  github_id: string | null
-  google_id: string | null
-  id: number
-  updated_at: string
-  username: string
+  created_at: string;
+  credits: Generated<number | null>;
+  email: string;
+  github_id: string | null;
+  google_id: string | null;
+  id: number;
+  updated_at: string;
+  username: string;
 }
 
 export interface DB {
-  images: Images
-  transactions: Transactions
-  users: Users
+  blurs: Blurs;
+  images: Images;
+  transactions: Transactions;
+  users: Users;
 }
