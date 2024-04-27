effd18ee65e105228d5f403582abf31b28a10b39
Matteo Gassend <mgassend@gmail.com>
2024-04-27 17:57:10 +0200

    add: blur index page wip

--- app/controllers/blurs_controller.ts
+++ app/controllers/blurs_controller.ts
@@ -1,13 +1,16 @@
 import { blurCreateValidator } from '#validators/blur_create'
-import { encode } from "blurhash"
+import { decode, encode } from "blurhash"
 import type { HttpContext } from '@adonisjs/core/http'
 import sharp from "sharp"
 import { db } from '#services/db'
 import app from '@adonisjs/core/services/app'
 
 export default class BlursController {
-  async index({ view }: HttpContext) {
-    return view.render("pages/app/blur/index")
+  async index({ auth, view }: HttpContext) {
+    const user = auth.getUserOrFail()
+    const blurs = await db.selectFrom("blurs").selectAll().where("user_id", "=", user.id).execute()
+
+    return view.render("pages/app/blur/index", { blurs })
   }
 
   async create({ view }: HttpContext) {
@@ -21,13 +24,14 @@ export default class BlursController {
     if (!payload.image.filePath) {
       return "ko";
     }
-    const encodedString = await new Promise((resolve, reject) => {
+    const encodedString: string = await new Promise((resolve, reject) => {
       sharp(payload.image.filePath).raw().ensureAlpha().toBuffer((err, buffer, { width, height }) => {
         if (err) return reject(err)
         resolve(encode(new Uint8ClampedArray(buffer), width, height, 4, 4))
       })
     })
-    await db.insertInto("blurs").values({ user_id: user.id, value: encodedString, created_at: new Date().getTime(), updated_at: new Date().getTime() }).execute()
+    const b64 = Buffer.from(decode(encodedString, 200, 200)).toString("base64")
+    await db.insertInto("blurs").values({ user_id: user.id, value: encodedString, b_64: b64, created_at: new Date().getTime(), updated_at: new Date().getTime() }).execute()
     return response.redirect().toRoute("app_blur.index")
   }
 }
