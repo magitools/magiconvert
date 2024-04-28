6a8b87c5f0a7e3be4b2c50ad53dd18870e928d00
Matteo Gassend <mgassend@gmail.com>
2024-04-28 16:32:03 +0200

    fix: resize canvas for image

--- resources/js/pages/blur/show.js
+++ resources/js/pages/blur/show.js
@@ -5,6 +5,9 @@ const metadata = canvas.getAttribute("data-meta")
 const parsedMeta = JSON.parse(metadata)
 const pixels = decode(hashValue, parsedMeta.width, parsedMeta.height)
 
+canvas.width = parsedMeta.width
+canvas.height = parsedMeta.height
+
 const ctx = canvas.getContext("2d")
 const imageData = ctx.createImageData(parsedMeta.width, parsedMeta.height)
 imageData.data.set(pixels)
