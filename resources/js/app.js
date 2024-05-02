054ddb5b9c7a203604831180067dad51abae651f
Matteo Gassend <mgassend@gmail.com>
2024-05-02 17:09:43 +0200

    add: unpoly + up compiler for canvas

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,22 @@
 import.meta.glob(["../images/**/*"])
-
+import { decode } from "blurhash"
+import "unpoly"
+import "unpoly/unpoly.css"
 import Alpine from 'alpinejs'
 window.Alpine = Alpine
 Alpine.start()
+
+up.compiler('canvas[data-hash]', (el) => {
+  const hashValue = el.getAttribute("data-hash")
+  const metadata = el.getAttribute("data-meta")
+  const parsedMeta = JSON.parse(metadata)
+  const pixels = decode(hashValue, parsedMeta.width, parsedMeta.height)
+
+  el.width = parsedMeta.width
+  el.height = parsedMeta.height
+
+  const ctx = el.getContext("2d")
+  const imageData = ctx.createImageData(parsedMeta.width, parsedMeta.height)
+  imageData.data.set(pixels)
+  ctx.putImageData(imageData, 0, 0)
+})
